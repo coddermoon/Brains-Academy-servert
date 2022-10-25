bd96f97d199eeb29e5c21dea2545d95134dda1f0
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-25 16:06:02 +0600

    primary course data added done

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const cors = require('cors')
-
+// export all dependencies datta
 const categories = require('./data/categories.json')
+const courses = require('./data/courses.json')
 const app = express()
 app.use(cors())
 const port = 5000
 
 app.get('/',(req,res)=>{
     res.send('this is homepage')
+})
+// coursesw
+app.get('/courses',(req,res)=>{
+    res.send(courses)
 })
 // courses page
 app.get('/categories',(req,res)=>{
