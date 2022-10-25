4f833ee1b4fe3618e98d82d6eb78f765090209de
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-10-25 15:30:08 +0600

    category loaded done

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const cors = require('cors')
 
+const categories = require('./data/categories.json')
 const app = express()
 app.use(cors())
 const port = 5000
 
 app.get('/',(req,res)=>{
     res.send('this is homepage')
+})
+// courses page
+app.get('/categories',(req,res)=>{
+    res.send(categories)
 })
 
 app.listen(port,()=>{
