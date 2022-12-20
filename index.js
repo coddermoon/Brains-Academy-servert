5ed72574dbf56301999584cfdbe49dc563c6bfc4
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-12-21 03:02:53 +0600

    ssl added

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
@@ -35,7 +35,45 @@ const run = async (req, res) => {
      app.post('/orders',async(req,res) => {
 
         const orderData = req.body
-        console.log(orderData);
+     
+
+        const data = {
+            total_amount: orderData.price,
+            currency: orderData.currency,
+            tran_id: new ObjectId().toString(), // use unique tran_id for each api call
+            success_url: 'http://localhost:3030/success',
+            fail_url: 'http://localhost:3030/fail',
+            cancel_url: 'http://localhost:3030/cancel',
+            ipn_url: 'http://localhost:3030/ipn',
+            shipping_method: 'Courier',
+            product_name: 'Computer.',
+            product_category: 'Electronic',
+            product_profile: 'general',
+            cus_name: orderData.name,
+            cus_email: orderData.email,
+            cus_add1: 'Dhaka',
+            cus_add2: 'Dhaka',
+            cus_city: 'Dhaka',
+            cus_state: 'Dhaka',
+            cus_postcode: '1000',
+            cus_country: 'Bangladesh',
+            cus_phone: '01711111111',
+            cus_fax: '01711111111',
+            ship_name: 'Customer Name',
+            ship_add1: 'Dhaka',
+            ship_add2: 'Dhaka',
+            ship_city: 'Dhaka',
+            ship_state: 'Dhaka',
+            ship_postcode: 1000,
+            ship_country: 'Bangladesh',
+        };
+        const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+        sslcz.init(data).then(apiResponse => {
+            // Redirect the user to payment gateway
+            let GatewayPageURL = apiResponse.GatewayPageURL
+            res.send({url:GatewayPageURL})
+            
+        });
         
 
 
